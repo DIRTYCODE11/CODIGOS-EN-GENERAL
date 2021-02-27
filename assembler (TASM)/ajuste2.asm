0c1b8669db0b0906eb982e678576838dc23f19af
DIRTYCODE11 <72365607+DIRTYCODE11@users.noreply.github.com>
2021-02-27 10:45:11 -0600

    Ajustes con ensamblador parte 2

--- assembler (TASM)/ajuste2.asm
+++ assembler (TASM)/ajuste2.asm
@@ -1,46 +1,48 @@
-; Con este ejemplo veremos como realizar ajustes para mostrar 
-; informacion en pantalla
-.286                ; set de instrucciones
-pila segment stack       		; segmento de pila
-	     db 32 DUP('stack--')
+; Ajustes parte 2 
+; este programa permite al usuario ingresar un numero por teclado
+; y mostrarse en pantalla usando los ajustes...
+
+.286
+pila segment stack  ; segmento de pila
+    db 32 DUP('stack--')
 pila ends
 
-datos segment                                        		; segmento de datos donde declaro mis variables
-	letrero1 db 10,13,'Ingresa un numero de un digito: $'
-	letrero2 db 10,13,'El numero es: $'
+datos segment ; segmento de datos
+    letrero1 db 10,13,'Ingresa un numero de un digito: $'
+    letrero2 db 10,13,'El numero es: $'
 datos ends
 
-codigo segment 'code'
+codigo segment 'code' ;segmento de codigo
 main proc FAR
-	       assume ss:pila, ds:datos, cs:codigo
+    assume ss:pila, ds:datos, cs:codigo ; le indica a ensamblador que es cada segmento
 
-	       mov    ax,datos                      ; muevo al registro ax lo que tiene datos
-	       mov    ds,ax                         ; muevo eso al segmento de datos
+    mov ax,datos
+    mov ds,ax
 
-	       mov    ah,09h                      	; mostrar Ingresemos un digito
-	       lea    dx, letrero1
-	       int    21h
+    mov ah,09h      ; mostrar Ingresemos un digito
+    lea dx, letrero1
+    int 21h
     
-	       mov    ah,01h                      	;solicito un caracter por pantalla
-	       int    21h
-	       sub    al,30h                      	; realizamos el ajuste
+    mov ah,01h      ;solicito un caracter
+    int 21h
+    sub al,30h      ; realizo el ajuste
     
-	       mov    cl,al                       	; lo guardo en el registro cl
+    mov cl,al       ; lo guardo en el registro cl, por convencion no es porque siga alguna norma
     
-	       mov    ah,09h                        ; imprimo el segundo letrero
-	       lea    dx, letrero2
-	       int    21h
+    mov ah,09h      ; imprimo el letrero 2
+    lea dx, letrero2
+    int 21h
     
-	       mov    ah,02h
-	       mov    dl,cl
-	       add    dl,30h                      	; realizo un ajuste para mostrar en pantalla
-	       int    21h
+    mov ah,02h      ; imprimo un caracter en pantalla
+    mov dl,cl       ; lo guardo en dl lo que tiene en cl, la funcion 02h nos dice que siempre usa el registro dl 
+    add dl,30h      ; para almacenar y de ahi realizamos el ajuste
+    int 21h
 
-	       mov    ah,4ch                      	;funcion que finaliza un programa
-	       int    21h
+    mov ah,4ch ;funcion que finaliza un programa 
+    int 21h
 
-	       ret
+    ret
 main endp
 codigo ends
 end main
-;------------------------------------------------DirtyCode 2021----------------------------------------------------
+;---------------------------------------------DirtyCode 2021----------------------------------------------
