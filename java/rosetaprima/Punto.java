b507f21b207b736d5dd4ada911148518442e660f
DIRTYCODE11 <72365607+DIRTYCODE11@users.noreply.github.com>
2021-09-23 14:42:22 -0500

    clase punto

---
+++ java/rosetaprima/Punto.java
@@ -0,0 +1,15 @@
+public class Punto {
+    private double x, y; // Valores de las cordenadas en X e Y
+    public Punto (double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+}
